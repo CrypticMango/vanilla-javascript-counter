62c9886677e81df821d927f419412d9ae88e17f7
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-25 13:51:01 -0600

    added variables

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //alert("hello bruddah");
 
+/* -create function that adds one to the number
+-create function that minuses one from the number
+-create a function that resets the number to zero */
+
+const minusButton = document.getElementById('minus-one');
+const addButton = document.getElementById('add-one');
+const resetButton = document.getElementById('reset');
+let counterNumber = document.getElementById('number');
+let number = 0;
