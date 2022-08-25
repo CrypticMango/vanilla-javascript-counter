6ee4ef94a4d3317197f396c8f7123bdc9b780357
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-25 14:30:53 -0600

    added functions for basic counter functions

--- app.js
+++ app.js
@@ -4,8 +4,35 @@
 -create function that minuses one from the number
 -create a function that resets the number to zero */
 
+//button variables
 const minusButton = document.getElementById('minus-one');
-const addButton = document.getElementById('add-one');
 const resetButton = document.getElementById('reset');
+const addButton = document.getElementById('add-one');
+
+//number variables
 let counterNumber = document.getElementById('number');
 let number = 0;
+
+console.log(counterNumber);
+console.log(number);
+
+minusButton.addEventListener('click', minusOne);
+
+addButton.addEventListener('click', addOne);
+
+resetButton.addEventListener('click', resetNumber);
+
+function addOne() {
+    //when button is pressed, add 1 to the number.
+    counterNumber.innerHTML = number++;
+}
+
+function minusOne() {
+    //when button is pressed, minus 1 from the number.
+    counterNumber.innerHTML = number--;
+}
+
+function resetNumber() {
+    //reset number back to zero when pressed.
+    counterNumber.innerHTML = 0;
+}
